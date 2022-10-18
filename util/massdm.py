568d1b07b9cee3c30bcdba17c8d73fdcea552b60
JaneHongEn <44541408+JaneHongEn@users.noreply.github.com>
2022-10-18 15:02:54 +0800

    Update massdm.py

--- util/massdm.py
+++ util/massdm.py
@@ -12,11 +12,12 @@ def MassDM(token, channels, Message):
     for channel in channels:
         for user in [x["username"]+"#"+x["discriminator"] for x in channel["recipients"]]:
             try:
-                setTitle(f"Messaging "+user)
-                requests.post(f'https://discord.com/api/v9/channels/'+channel['id']+'/messages',
-                    proxies={"http": f'{proxy()}'},
-                    headers={'Authorization': token},
-                    data={"content": f"{Message}"})
-                print(f"{Fore.RED}Messaged: {Fore.WHITE}"+user+Fore.RESET)
+                while True:
+                    setTitle(f"Messaging "+user)
+                    requests.post(f'https://discord.com/api/v9/channels/'+channel['id']+'/messages',
+                        proxies={"http": f'{proxy()}'},
+                        headers={'Authorization': token},
+                        data={"content": f"{Message}"})
+                    print(f"{Fore.RED}Messaged: {Fore.WHITE}"+user+Fore.RESET)
             except Exception as e:
                 print(f"The following error has been encountered and is being ignored: {e}")
